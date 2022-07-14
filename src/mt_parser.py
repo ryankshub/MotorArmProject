53f882451f9036eabf9d8b1c52b5b2b30237ce10
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2022-07-14 00:03:40 -0500

    Added some plotting tools for analysis acceleration signals and step counter, Altered test script to try various techniques of soothing the curves

--- src/mt_parser.py
+++ src/mt_parser.py
@@ -5,8 +5,10 @@
 # Python import
 
 # 3rd-party import
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import signal
 
 def parse_mt(filepath):
     """
@@ -16,14 +18,14 @@ def parse_mt(filepath):
         string - filepath: filepath to MT .txt file
     
     Return:
-        dict of 7 arrays with 'Time_ms', 'AccX', 'AccY', 'AccZ', 
+        dict of 7 arrays with 'Time_s', 'AccX', 'AccY', 'AccZ', 
             'FreeAccX', 'FreeAccY', and 'FreeAccZ'
     """
     df = pd.read_csv(filepath, skiprows=4, sep='\t')
-    # Output data rate is 100Hz or 10ms between samples
+    # Output data rate is 100Hz or 0.01s between samples
     start_sample = df['PacketCounter'][0]
-    df['Time_ms'] = [10 * (df['PacketCounter'][i] - start_sample) for i in range(len(df['PacketCounter']))]
-    rtn_dic = {'Time_ms': df['Time_ms'].values,
+    df['Time_s'] = [0.01 * (df['PacketCounter'][i] - start_sample) for i in range(len(df['PacketCounter']))]
+    rtn_dic = {'Time_s': df['Time_s'].values,
                'AccX': df['Acc_X'].values,
                'AccY': df['Acc_Y'].values,
                'AccZ': df['Acc_Z'].values,
@@ -33,43 +35,86 @@ def parse_mt(filepath):
     return rtn_dic
 
 
-def plot_mt_data(filepath, title):
+def plot_all_mt_data(filepath, title, psd_plot=True):
     """
-    Plot data from MT IMU Software.
+    Plot acceleration from all 3 axis from MT IMU Software.
+
+    Args:
+        string filepath - filepath of MT data file
+        string title - Title for plot
+        bool psd_plot - Set true to plot acceleration alongside frequency domain, 
+                        false to plot alongside free accel (w/o gravity)
     """
     data_dict = parse_mt(filepath)
 
     fig, axs = plt.subplots(3,2)
     fig.suptitle(title)
-    # Set up for Acc X
-    axs[0][0].plot(data_dict['Time_ms'], data_dict["AccX"], color="tab:red")
-    axs[0][0].set_xlabel("Time(ms)")
-    axs[0][0].set_ylabel("Acc X(m/s^2)")
+    fs = 1/np.mean(np.diff(data_dict['Time_s']))
 
-    # Set up for Free Acc X (no gravity)
-    axs[0][1].plot(data_dict['Time_ms'], data_dict["FreeAccX"], color="tab:red")
-    axs[0][1].set_xlabel("Time(ms)")
-    axs[0][1].set_ylabel("Free Acc X(m/s^2)")
+    # Set up for Acc X
+    axs[0][0].plot(data_dict['Time_s'], data_dict["AccX"], color="tab:red")
+    #axs[0][0].set_xlabel("Time(ms)")
+    axs[0][0].set_ylabel("Acc X")
+    axs[0][0].set_ylim([-14,14])
+
+    
+    if (psd_plot):
+        # Plot Freq domain
+        nPts = len(data_dict["AccX"])
+        f, Pxx = signal.welch(data_dict['AccX'], fs, nperseg=nPts)
+
+        axs[0][1].plot(f, Pxx, color="tab:red")
+
+        #axs[0][1].set_xlabel("Freq(hz)")
+        axs[0][1].set_ylabel("Power")
+        axs[0][1].set_xlim([0,10])
+    else:
+        # Set up for Free Acc X (no gravity)
+        axs[0][1].plot(data_dict['Time_s'], data_dict["FreeAccX"], color="tab:red")
+        #axs[0][1].set_xlabel("Time(ms)")
+        axs[0][1].set_ylabel("Free Acc X")
 
     # Set up for AccY 
-    axs[1][0].plot(data_dict['Time_ms'], data_dict["AccY"], color="tab:blue")
-    axs[1][0].set_xlabel("Time(ms)")
-    axs[1][0].set_ylabel("Acc Y(m/s^2)")
+    axs[1][0].plot(data_dict['Time_s'], data_dict["AccY"], color="tab:blue")
+    #axs[1][0].set_xlabel("Time(ms)")
+    axs[1][0].set_ylabel("Acc Y")
+    axs[1][0].set_ylim([-14,14])
 
-    # Set up for Free Acc Y
-    axs[1][1].plot(data_dict['Time_ms'], data_dict["FreeAccY"], color="tab:blue")
-    axs[1][1].set_xlabel("Time(ms)")
-    axs[1][1].set_ylabel("Free Acc Y(m/s^2)")
+    if (psd_plot):
+        # Plot Freq domain
+        nPts = len(data_dict["AccY"])
+        f, Pxx = signal.welch(data_dict['AccY'], fs, nperseg=nPts)
+
+        axs[1][1].plot(f, Pxx, color="tab:blue")
+        #axs[1][1].set_xlabel("Freq(hz)")
+        axs[1][1].set_ylabel("Power")
+        axs[1][1].set_xlim([0,10])
+    else:
+        # Set up for Free Acc Y
+        axs[1][1].plot(data_dict['Time_s'], data_dict["FreeAccY"], color="tab:blue")
+        #axs[1][1].set_xlabel("Time(ms)")
+        axs[1][1].set_ylabel("Free Acc Y")
 
     # Set up for AccZ
-    axs[2][0].plot(data_dict['Time_ms'], data_dict["AccZ"], color="tab:green")
+    axs[2][0].plot(data_dict['Time_s'], data_dict["AccZ"], color="tab:green")
     axs[2][0].set_xlabel("Time(ms)")
-    axs[2][0].set_ylabel("Acc Z(m/s^2)")
+    axs[2][0].set_ylabel("Acc Z")
+    axs[2][0].set_ylim([-14,14])
 
     # Set up for Free Acc Z
-    axs[2][1].plot(data_dict['Time_ms'], data_dict["FreeAccX"], color="tab:green")
-    axs[2][1].set_xlabel("Time(ms)")
-    axs[2][1].set_ylabel("Free Acc Z(m/s^2)")
+    if (psd_plot):
+        # Plot Freq domain
+        nPts = len(data_dict["AccZ"])
+        f, Pxx = signal.welch(data_dict['AccZ'], fs, nperseg=nPts)
+
+        axs[2][1].plot(f, Pxx, color="tab:green")
+        axs[2][1].set_xlabel("Freq(hz)")
+        axs[2][1].set_ylabel("Power")
+        axs[2][1].set_xlim([0,10])
+    else:
+        axs[2][1].plot(data_dict['Time_s'], data_dict["FreeAccX"], color="tab:green")
+        axs[2][1].set_xlabel("Time(ms)")
+        axs[2][1].set_ylabel("Free Acc Z(m/s^2)")
 
 
     plt.subplots_adjust(left=0.1,
@@ -79,3 +124,76 @@ def plot_mt_data(filepath, title):
                     wspace=0.4, 
                     hspace=0.6)
     plt.show()
+
+
+def plot_mt_data(filepath, title, key, psd_plot=True):
+    """
+    Plot subset of data from MT IMU Software
+
+    Args:
+        string filepath - filepath to MT data file
+        string title - title of plot
+        string or array key - If key is a string, parses mt data file and pulls
+            data from corresponding key. If key is not a string, assumed to be 
+            raw_data
+        bool psd_plot - set true to plot psd alongside acceleration
+    """ 
+    data_dict = parse_mt(filepath)
+
+    if (psd_plot):
+        fig, axs = plt.subplots(1,2)
+        fig.suptitle(title)
+        fs = 1/np.mean(np.diff(data_dict['Time_s']))
+
+        # Set up for Acc X
+        accel_data = 0
+        if (type(key) == str):
+            accel_data = data_dict[key]
+        else:
+            accel_data = key
+
+        axs[0].plot(data_dict['Time_s'], accel_data, color="tab:blue")
+        axs[0].set_xlabel("Time(ms)")
+        axs[0].set_ylabel("AccY")
+        #axs[0][0].set_ylim([-14,14])
+
+        # Plot Freq domain
+        nPts = len(accel_data)
+        f, Pxx = signal.welch(accel_data, fs, nperseg=nPts)
+
+        axs[1].plot(f, Pxx, color="tab:red")
+        axs[1].set_xlabel("Freq(hz)")
+        axs[1].set_ylabel("Power")
+        axs[1].set_xlim([0,10])
+    
+    else:
+        fig, ax = plt.subplots(1,1)
+        fig.suptitle(title)
+        ax.plot(data_dict['Time_s'], accel_data, color="tab:blue")
+        ax.set_xlabel("Time(ms)")
+        ax.set_ylabel(key)
+
+    plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.6)
+    plt.show()
+
+
+def apply_filter(data, filter_order, filter_type, cutoff_freq):
+    """
+    Apply butterworth filter of certain type
+
+    Args:
+        np.array data - raw data
+        int filter_order - order of the butterworth filter
+        string filter_type - type of filter: options are 'lowpass', 'highpass',
+            'bandpass', or 'bandstop' 
+        float(s) cutoff_freq - cutoff frequency for the filter. For 'lowpass' 
+            and 'highpass' should be a scalar; for 'bandpass' or 'bandstop', the 
+            frequency should be a two-elem list
+    """
+    b, a = signal.butter(filter_order, cutoff_freq, filter_type)
+    return signal.lfilter(b, a, data)
--- src/test_cadence_tracker.py
+++ src/test_cadence_tracker.py
@@ -7,25 +7,42 @@ import mt_parser
 # Python import
 
 # 3rd-party import
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
 
 # Test file for CadenceTracker
 # TODO: add this to unit test folder
 
-CT_50 = CadenceTracker(freq_Hz = 500, time_window_ms=50)
-CT_100 = CadenceTracker(freq_Hz = 500, time_window_ms=100)
-CT_200 = CadenceTracker(freq_Hz = 500, time_window_ms=200)
+# CT_50 = CadenceTracker(freq_Hz = 500, time_window_ms=50)
+# CT_100 = CadenceTracker(freq_Hz = 500, time_window_ms=100)
+# CT_200 = CadenceTracker(freq_Hz = 500, time_window_ms=200)
 
-CT_50.add_measurement(9.81)
-print(f"CT_50 data: {CT_50._data}")
-CT_200.add_datum([9.81, 0, 6])
-print(f"CT_200 data: {CT_200._data}")
-CT_200.clear_data()
-print(f"CT_200 data: {CT_200._data}")
-test = [1.1*i for i in range(30)]
-CT_50.add_datum(test)
-print(f"CT_50 data: {CT_50._data}")
-print(f"CT_50 cadence: {CT_50.calculate_cadence()}")
-print(f"CT_100 cadence: {CT_100.calculate_cadence()}")
+# CT_50.add_measurement(9.81)
+# print(f"CT_50 data: {CT_50._data}")
+# CT_200.add_datum([9.81, 0, 6])
+# print(f"CT_200 data: {CT_200._data}")
+# CT_200.clear_data()
+# print(f"CT_200 data: {CT_200._data}")
+# test = [1.1*i for i in range(30)]
+# CT_50.add_datum(test)
+# print(f"CT_50 data: {CT_50._data}")
+# print(f"CT_50 cadence: {CT_50.calculate_cadence()}")
+# print(f"CT_100 cadence: {CT_100.calculate_cadence()}")
 
 filepath = '../data/cadence_test_data/MT_012006A8-2022-07-12-16h40_000_36-000.txt'
-mt_parser.plot_mt_data(filepath, "TEST")
+#mt_parser.plot_all_mt_data(filepath, "Raw Acceleration and PSD plots for 0.5ms stride")
+data_dict = mt_parser.parse_mt(filepath)
+time_steps = data_dict['Time_s']
+raw_data = data_dict['AccY']
+#mt_parser.plot_mt_data(filepath, "Raw Y-Acceleration and PSD plots for 0.5ms stride", "AccY")
+#fs = 1/np.mean(np.diff(data_dict['Time_s']))
+#filt_data = mt_parser.apply_filter(data_dict['AccY'], 3, 'lowpass', 0.9)
+#mt_parser.plot_mt_data(filepath, "Filtered Y-Acceleration and PSD plots for 0.5ms stride", filt_data)
+raw_data_sq = raw_data*-1#np.power(raw_data,2)
+
+peaks, _ = signal.find_peaks(raw_data_sq)
+plt.plot(raw_data_sq)
+plt.plot(peaks, raw_data_sq[peaks], "x")
+print(f"Number of steps {len(peaks)}")
+plt.show()
