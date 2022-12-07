e785bbb268ff1a06aad2270bbd379523a7fd76bf
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2022-12-07 17:38:10 -0600

    Debugged Trajectory Spline Generator, tested it with pendulumGui, looks good

--- src/traj_spline_gen.py
+++ src/traj_spline_gen.py
@@ -26,7 +26,7 @@ class TrajectorySplineGenerator:
         self._angle = 0
         self.time_till_step = -1
         self.sample_rate = sample_rate
-        self.trajectory = deque()
+        self._trajectory = deque()
         self._HOME_RATE = .001 # in rev/sample
         self.RETRACT_ANGLE = retract_angle
         self._DEGREE_THRES = .005 # in rev
@@ -47,14 +47,17 @@ class TrajectorySplineGenerator:
         """
         # Mod value to keep it between -0.5 and 0.5
         self._angle = value % 1
-        if self._angle > .5
+        if self._angle > .5:
             self._angle -= 1
 
 
     def get_degree_range(self, steps, time_window):
         """
         """
-        return -20/360
+        if steps > 5:
+            return -20/360
+        else:
+            return -30/360
 
 
     def generate_trajectory(self, target_angle, current_angle):
@@ -64,23 +67,28 @@ class TrajectorySplineGenerator:
         # Get max velocity
         angle_disp = target_angle - current_angle
         max_vel = 2*angle_disp/self.time_till_step
-        
+        print(f"ANGLE_DIPS {angle_disp}, MAX_VEL {max_vel}")
         # Get the point where trajectory should hit maximum velocity
         # For this algorithm it's the half-way point
-        half_samp = self.time_till_step*self.sample_rate*.5
+        half_samp = int(self.time_till_step*self.sample_rate*.5)
         half_vel_traj = np.linspace(0, max_vel, half_samp, endpoint=False)
-
+        print(f"HALF_SAMP {half_samp}")
         # Combine velocities
         vel_traj = np.concatenate((half_vel_traj, 
                                    np.array([max_vel]),
                                    np.flip(half_vel_traj)))
+        vel_traj = vel_traj/self.sample_rate
+        print(f"VEL_TRAJ {vel_traj}, {sum(vel_traj)}")
         # Make trajectory
         angle_traj = []
         cand_angle = current_angle
+        print(f"First CAND_ANGLE {cand_angle}")
         for vel in vel_traj:
             cand_angle += vel
-            angle_traj.append(angle_traj)
+            angle_traj.append(cand_angle)
+
         
+        print(f"ANGLE_TRAJ {angle_traj}")
         return deque(angle_traj)
 
     
@@ -101,7 +109,7 @@ class TrajectorySplineGenerator:
         # Non walking behavior
         if steps == -1: 
             self._trajectory.clear() 
-            if abs(self._angle) < self.DEGREE_THRES:
+            if abs(self._angle) < self._DEGREE_THRES:
                 return self._angle
             elif self._angle < 0.0:
                 self.current_angle = (self._angle + self._HOME_RATE)
@@ -116,19 +124,14 @@ class TrajectorySplineGenerator:
             # Get degree range
             degree_range = self.get_degree_range(steps, time_window)
             # Generate new trajectory
-            if abs(degree_range - self._angle) < self.DEGREE_THRES:
+            if abs(degree_range - self._angle) < self._DEGREE_THRES:
                 # Swing backward
+                print("SWING BACK")
                 self._trajectory = self.generate_trajectory(self.RETRACT_ANGLE,
                                                             self._angle)
             else:
                 # Swing Forward
+                print("SWING FORWARD")
                 self._trajectory = self.generate_trajectory(degree_range, 
                                                             self._angle)
         return self._trajectory.popleft()
-
-
-        
-
-
-
-
